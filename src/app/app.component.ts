4c11c14565f8c7bd12f3679f78e6572225cca228
voilacf <vio.lafe.vf@gmail.com>
2022-03-25 14:35:58 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,39 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+/*@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.ts',
+  styleUrls: ['./item.component.css']
+})*/
 export class AppComponent {
   title = 'todo';
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    {description: 'eat', done: true},
+    {description: 'sleep', done: false},
+    {description: 'play', done: false},
+    {description: 'laugh', done: false},
+  ];
+
+  get items(){
+    if (this.filter === 'all'){
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+  
+  //instead of unshift() which adds a new item at the beginnig of the list, 
+  //push(), that adds a new item at the end of the list, can be used too
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  } 
+
+  remove(item){
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
 }
